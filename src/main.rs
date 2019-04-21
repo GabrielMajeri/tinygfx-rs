927c2d9a8a3a06c3a63188b0789331d9e5cc68ad
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-04-21 09:15:25 +0300

    Display rendered image through a Piston window

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,16 @@ fn quantize(pixel: &image::Rgb<f32>) -> image::Rgb<u8> {
     ])
 }
 
+/// Converts a RGB pixel to a fully opaque RGBA pixel.
+fn to_rgba(pixel: image::Rgb<u8>) -> image::Rgba<u8> {
+    image::Rgba([pixel.data[0], pixel.data[1], pixel.data[2], 255u8])
+}
+
 use image::ImageBuffer;
 type Pixel = image::Rgb<f32>;
 type RenderBuffer = ImageBuffer<Pixel, Vec<f32>>;
 
+/// Renders the scene to a given buffer image.
 fn render(buffer: &mut RenderBuffer) {
     let width = buffer.width();
     let height = buffer.height();
@@ -42,10 +48,24 @@ fn render(buffer: &mut RenderBuffer) {
     });
 }
 
+use piston_window::{EventLoop, OpenGL, PistonWindow, Texture, TextureSettings, WindowSettings};
+
 fn main() {
     // Configurable dimensions for the canvas
     let width = 1024;
     let height = 768;
+
+    let window_size = (width, height);
+    let opengl_version = OpenGL::V3_2;
+    let mut window = WindowSettings::new("Software Raytracer", window_size)
+        .exit_on_esc(true)
+        .opengl(opengl_version)
+        .resizable(false)
+        .build::<PistonWindow>()
+        .expect("Failed to create Piston window");
+
+    window.set_lazy(true);
+
     // Solid black
     let background_color = image::Rgb([0.0f32; 3]);
 
@@ -53,12 +73,16 @@ fn main() {
 
     render(&mut framebuffer);
 
-    // Save the rendered frame to disk
-    let convert_pixel = |x, y| quantize(framebuffer.get_pixel(x, y));
+    let convert_pixel = |x, y| to_rgba(quantize(framebuffer.get_pixel(x, y)));
     let output_buffer = ImageBuffer::from_fn(width, height, convert_pixel);
 
-    let output_path = "output.png";
-    output_buffer
-        .save(output_path)
-        .expect("Failed to save generated framebuffer to file");
+    let buffer_texture =
+        Texture::from_image(&mut window.factory, &output_buffer, &TextureSettings::new())
+            .expect("Failed to create texture from back buffer");
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |ctx, gfx| {
+            piston_window::image(&buffer_texture, ctx.transform, gfx);
+        });
+    }
 }
